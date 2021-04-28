08e2fcb31294907ddfed0a5505bce484d997e0a7
Daniela0209 <grajalesdaniela253@gmail.com>
2021-04-27 22:10:13 -0500

    se agrego la implementacion de firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Vista from "./pages/Vista";
+import firebase from 'firebase/app';
 
+firebase.initializeApp({
+  apiKey: "AIzaSyBHq9LCXTvKWv8a5giOFY1T7cI3ETYnSW4",
+  authDomain: "notification-d3d10.firebaseapp.com",
+  projectId: "notification-d3d10",
+  storageBucket: "notification-d3d10.appspot.com",
+  messagingSenderId: "34715758033",
+  appId: "1:34715758033:web:efbc6b2a0a0ddf285644f0"
+});
 
 function App() {
   return (
